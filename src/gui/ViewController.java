256a037c089a87ca22724790f37bd039831f100e
JonasMoura-J <jonasmourat9090@gmail.com>
2020-11-23 17:06:42 -0300

    Adicionando metodos da calculadora

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,17 +1,55 @@
 package gui;
 
+import java.util.Locale;
+
 import gui.util.Alerts;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class ViewController {
+	@FXML
+	private TextField txtNumero1;
 	
 	@FXML
-	private Button btn;
+	private TextField txtNumero2;
 	
 	@FXML
-	public void onBtnAction() {
-		Alerts .showAlert("Titulo", null, "Hello World!", AlertType.ERROR);
+	private TextField txtOperacao;
+	@FXML
+	private Label lblResult;
+	
+	@FXML
+	private Button btnCalc;
+	
+	@FXML
+	public void onBtnCalcAction() {
+		try {
+			Locale.setDefault(Locale.US);
+			double num1 = Double.parseDouble(txtNumero1.getText());
+			double num2 = Double.parseDouble(txtNumero2.getText());
+			double result = 0;
+			
+			switch(txtOperacao.getText()) {
+				case "+":
+					result = num1 + num2;
+					break;
+				case "-":
+					result = num1 - num2;
+					break;
+				case "*":
+					result = num1 * num2;
+					break;
+				case "/":
+					result = num1 / num2;
+					break;
+			}
+			
+			lblResult.setText(String.format("%.2f", result));
+		}catch(NumberFormatException e) {
+			Alerts.showAlert("Error", null, e.getMessage(), AlertType.ERROR);
+		}
 	}
 }
